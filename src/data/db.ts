c2c01dfbe92d485d42ec224c93779517c669ed53
strangeal <bajerijustice@gmail.com>
2023-04-03 23:51:59 +0000

    Modify db.ts to add recipe list data

--- src/data/db.ts
+++ src/data/db.ts
@@ -25,20 +25,82 @@ export const productList = [
     label: "San Francisco – Oakland Bay Bridge, United States",
     imgPath:
       "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+      id: 1,
   },
   {
     label: "Bird",
     imgPath:
       "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+      id: 2,
   },
   {
     label: "Bali, Indonesia",
     imgPath:
       "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+      id: 3,
   },
   {
     label: "Goč, Serbia",
     imgPath:
       "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+      id: 4,
+  },
+];
+
+export const recipeList =[
+  {
+    recipe_name: "Spicy Thai Noodle Soup",
+    cooking_time: "30 minutes",
+    recipe_image:
+      "https://www.everydayeasyeats.com/wp-content/uploads/2017/01/Thai-Spicy-Noodle-Soup-4.jpg",
+    likes: 125,
+    rating: 4.8,
+      chef_image: "https://img.freepik.com/free-photo/young-cook-female-wearing-chef-uniform-holding-isolated-orange-wall_141793-36632.jpg?",
+      chef_name: "John Smith",
+    posted_time: "15 hours ago",
+  },
+  {
+    recipe_name: "Garlic Butter Shrimp Scampi",
+    cooking_time: "20 minutes",
+    recipe_image:
+      "https://www.wholesomeyum.com/wp-content/uploads/2022/01/wholesomeyum-Lemon-Garlic-Butter-Shrimp-Recipe-19.jpg",
+    likes: 238,
+    rating: 4.9,
+    chef_image: "https://img.freepik.com/free-photo/smiling-young-male-cook-wearing-chef-uniform-glasses-isolated-white-wall_141793-78421.jpg?",
+    chef_name: "David Lee",
+    posted_time: "5 hours ago",
+  },
+  {
+    recipe_name: "Creamy Mushroom Chicken",
+    cooking_time: "45 minutes",
+    recipe_image:
+      "https://cook.me/wp-content/uploads/2019/11/Roast-Chicken-and-Vegetables-Recipe-How-To-Make-Roast-Chicken-and-Vegetables-Delicious-Roast-Chicken-and-Vegetables-14-678x1024.jpg",
+    likes: 312,
+    rating: 4.7,
+    chef_image: "https://img.freepik.com/free-photo/smiling-young-male-cook-wearing-chef-uniform-glasses-isolated-white-wall_141793-78421.jpg?",
+    chef_name: "Emily Jones",
+    posted_time: "15 hours ago",
+  },
+  {
+    recipe_name: "Homemade Beef Stroganoff",
+    cooking_time: "4 hour",
+    recipe_image:
+      "https://www.thecookierookie.com/wp-content/uploads/2021/12/featured-beef-stroganoff-recipe.jpg",
+    likes: 410,
+    rating: 4.6,
+    chef_image: "https://img.freepik.com/free-photo/young-cook-female-wearing-chef-uniform-holding-isolated-orange-wall_141793-36632.jpg?",
+    chef_name: "Sophia Chen",
+    posted_time: "1 hours ago",
+  },
+  {
+    recipe_name: "Roasted Garlic and Tomato Soup",
+    cooking_time: "40 minutes",
+    recipe_image:
+      "https://lucyandlentils.co.uk/wp-content/uploads/2022/11/IMG_7510.jpg",
+    likes: 178,
+    rating: 4.8,
+    chef_image: "https://img.freepik.com/free-photo/smiling-young-male-cook-wearing-chef-uniform-glasses-isolated-white-wall_141793-78421.jpg?",
+    chef_name: "Daniel Kim",
+    posted_time: "2 hours ago",
   },
 ];
