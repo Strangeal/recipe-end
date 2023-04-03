fb3f6c2cfe0786696daaea9bcd2edd84a68aa017
strangeal <bajerijustice@gmail.com>
2023-04-03 23:49:11 +0000

    Modify home component to import Healineslide to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,17 @@
 import { Box } from "@mui/system";
-import { productList } from "../data/db";
+import { productList, recipeList } from "../data/db";
 import Headline from "./Headline";
+import HeadlineSlides from "./HeadlineSlides";
 import Slides from "./Slides";
 
 const Home = () => {
   return (
     <Box>
-      <Headline />
-      <Slides products={productList} />
-      Welcome to the Home page
+      <HeadlineSlides recipes={recipeList} />
+      <Box sx={{ m: 1 }}>
+        Today's Recipe
+        <Slides products={productList} />
+      </Box>
     </Box>
   );
 };
