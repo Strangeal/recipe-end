60cd5cc6c4dbb8903ef1a0616c58b8fc635e21e1
strangeal <bajerijustice@gmail.com>
2023-04-05 15:32:28 +0000

    Render instances of genslides and pass props data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,18 @@
 import { Box } from "@mui/system";
-import { productList, recipeList } from "../data/db";
+import {
+  productList,
+  recipeList,
+  breakFastList,
+  dinnerList,
+  launchList,
+} from "../data/db";
+import GenSlides from "./GenSlides";
+import FoodCategory from "./categories/FoodCategory";
 import Headline from "./Headline";
 import HeadlineSlides from "./HeadlineSlides";
 import Slides from "./Slides";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Home = () => {
   return (
@@ -12,6 +22,40 @@ const Home = () => {
         Today's Recipe
         <Slides products={productList} />
       </Box>
+      <>
+        <Box sx={{ m: 1 }}>
+          <div className="category-header">
+            <h5>Breakfast</h5>
+            <Link to="">See All</Link>
+          </div>
+          <GenSlides data={breakFastList} />
+        </Box>
+        <Box sx={{ m: 1 }}>
+          <div className="category-header">
+            <h5>Dinner</h5>
+            <Link to="">See All</Link>
+          </div>
+          <GenSlides data={dinnerList} />
+        </Box>
+        <Box sx={{ m: 1 }}>
+          <div className="category-header">
+            <h5>Launch</h5>
+            <Link to="">See All</Link>
+          </div>
+          <GenSlides data={launchList} />
+        </Box>
+      </>
+      <Button
+        sx={{
+          // mx: 20,
+          width: "100%",
+          color: "#fff",
+          bgcolor: "#ED6D06",
+          textAlign: "center",
+        }}
+      >
+        Make A Collection
+      </Button>
     </Box>
   );
 };
