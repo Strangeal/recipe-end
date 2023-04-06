c1afd2f408dc9c6f4e173facaf2e32ac3f46f9cf
strangeal <bajerijustice@gmail.com>
2023-04-06 16:03:47 +0000

    Import details component in breakfast for testing

--- src/components/categories/BreakFast.tsx
+++ src/components/categories/BreakFast.tsx
@@ -1,9 +1,11 @@
 import Populate from "./Populate";
 import { breakFastList } from "../../data/db";
+import Details from "./Details";
 
 const BreakFast = () => {
   return (
     <>
+      <Details />
       <Populate data={breakFastList} />
     </>
   );
