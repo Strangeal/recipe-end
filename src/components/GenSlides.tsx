c366a928a97f1c54df860dce1a10f5e9169f684d
strangeal <bajerijustice@gmail.com>
2023-04-06 07:53:14 +0000

    Modify wrap element to use fragment

--- src/components/GenSlides.tsx
+++ src/components/GenSlides.tsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/autoplay";
 import "swiper/css";
-import { Autoplay } from "swiper";
 import FoodCategory from "./categories/FoodCategory";
 
 type BreakFastProps = {
@@ -13,7 +12,7 @@ type BreakFastProps = {
 };
 const BreakFastSlides = ({ data }: BreakFastProps) => {
   return (
-    <div>
+    <>
       <Swiper spaceBetween={10} slidesPerView={2.1}>
         {data &&
           data.map((item, index) => (
@@ -22,7 +21,7 @@ const BreakFastSlides = ({ data }: BreakFastProps) => {
             </SwiperSlide>
           ))}
       </Swiper>
-    </div>
+    </>
   );
 };
 
