5e4350f3a11604c5172f35c3b96bb6738f4bd6a0
strangeal <bajerijustice@gmail.com>
2023-04-07 14:21:00 +0000

    Modify data

--- src/data/db.ts
+++ src/data/db.ts
@@ -22,26 +22,31 @@ export const menuItems = [
 
 export const productList = [
   {
+    id: 1,
     label: "San Francisco – Oakland Bay Bridge, United States",
     imgPath:
       "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680769431/Recipe-App/recipes/micheile-henderson-qw3AyjHNF_k-unsplash_sqv6yb.jpg",
   },
   {
+    id: 2,
     label: "Bird",
     imgPath:
       "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680769903/Recipe-App/recipes/aleisha-kalina-Gi779Hq8THQ-unsplash_ukhm84.jpg",
   },
   {
+    id: 3,
     label: "Bali, Indonesia",
     imgPath:
       "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680769427/Recipe-App/recipes/micheile-henderson-mQSztA48y6A-unsplash_yj6rgw.jpg",
   },
   {
+    id: 4,
     label: "Goč, Serbia",
     imgPath:
       "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680769370/Recipe-App/recipes/brooke-lark-qdyBKWSzpSI-unsplash_guyueg.jpg",
   },
   {
+    id: 5,
     label: "Goč, Serbia",
     imgPath:
       "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680769768/Recipe-App/recipes/jane-4AFO0pMeBfU-unsplash_r59xxi.jpg",
@@ -50,6 +55,7 @@ export const productList = [
 
 export const recipeList =[
   {
+    id: 1,
     recipe_name: "Spicy Thai Noodle Soup",
     cooking_time: "30 minutes",
     recipe_image:
@@ -61,6 +67,7 @@ export const recipeList =[
     posted_time: "15 hours ago",
   },
   {
+    id: 2,
     recipe_name: "Garlic Butter Shrimp Scampi",
     cooking_time: "20 minutes",
     recipe_image:
@@ -72,6 +79,7 @@ export const recipeList =[
     posted_time: "5 hours ago",
   },
   {
+    id: 3,
     recipe_name: "Creamy Mushroom Chicken",
     cooking_time: "45 minutes",
     recipe_image:
@@ -83,6 +91,7 @@ export const recipeList =[
     posted_time: "15 hours ago",
   },
   {
+    id: 4,
     recipe_name: "Homemade Beef Stroganoff",
     cooking_time: "4 hour",
     recipe_image:
@@ -94,6 +103,7 @@ export const recipeList =[
     posted_time: "1 hours ago",
   },
   {
+    id: 5,
     recipe_name: "Roasted Garlic and Tomato Soup",
     cooking_time: "40 minutes",
     recipe_image:
@@ -108,84 +118,114 @@ export const recipeList =[
 
 export const breakFastList = [
   {
+    id: 1,
     name: "Avocado Toast",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680767841/Recipe-App/breakfast/david-b-townsend-IQSDSFO9UuI-unsplash_swtp1l.jpg",
     cal: 300,
+    category: 'breakfast'
   },
   {
+    id: 2,
     name: "Greek Yogurt",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768055/Recipe-App/breakfast/ben-kolde-FFqNATH27EM-unsplash_rzhork.jpg",
     cal: 200,
+    category: 'breakfast'
   },
   {
+    id: 3,
     name: "Oatmeal Nuts",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768057/Recipe-App/breakfast/joseph-gonzalez-zcUgjyqEwe8-unsplash_lfstp7.jpg",
     cal: 250,
+    category: 'breakfast'
   },
   {
+    id: 4,
     name: "Egg Veggie",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768105/Recipe-App/breakfast/eiliv-aceron-uAm1CZMdPCw-unsplash_rtt7cd.jpg",
     cal: 350,
+    category: 'breakfast'
   },
   {
+    id: 5,
     name: "Banana Smoothie",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768060/Recipe-App/breakfast/chris-ralston-09HGdZzkP-Q-unsplash_1_ygurwe.jpg",
     cal: 400,
+    category: 'breakfast'
   },
 ];
 
 export const dinnerList = [
   {
+    id: 1,
     name: "Grilled Salmon",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768634/Recipe-App/dinner/peter-dawn-DzuzZBlIcsM-unsplash_mcepef.jpg",
     cal: 300,
+    category: "dinner",
   },
   {
+    id: 2,
     name: "Roasted Chicken",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768630/Recipe-App/dinner/hoja-studio-Thw3nleO3cM-unsplash_fx0hw1.jpg",
     cal: 350,
+    category: "dinner",
   },
   {
+    id: 3,
     name: "Vegetable Stir Fry",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768539/Recipe-App/dinner/casey-lee-awj7sRviVXo-unsplash_i21db3.jpg",
     cal: 250,
+    category: "dinner",
   },
   {
+    id: 4,
     name: "Spaghetti Meatballs",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768559/Recipe-App/dinner/ruyan-ayten-gUFI8SxNvxo-unsplash_alhy0b.jpg",
     cal: 400,
+    category: "dinner",
   },
   {
+    id: 5,
     name: "Grilled Steak",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768627/Recipe-App/dinner/chumil-photo-TK5VlaGqkZc-unsplash_1_vlkaio.jpg",
     cal: 500,
+    category: "dinner",
   },
 ];
 
 export const lunchList = [
   {
+    id: 1,
     name: "Grilled Salmon",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768288/Recipe-App/lunch/monika-grabkowska-pCxJvSeSB5A-unsplash_foecvh.jpg",
     cal: 350,
+    category: "lunch",
   },
   {
+    id: 2,
     name: "Mediterranean Salad",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768293/Recipe-App/lunch/mariana-medvedeva-fk6IiypMWss-unsplash_qdhqnd.jpg",
     cal: 280,
+    category: "lunch",
   },
   {
+    id: 3,
     name: "Pesto Pasta",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768299/Recipe-App/lunch/chumil-photo-TK5VlaGqkZc-unsplash_fejxyu.jpg",
     cal: 420,
+    category: "lunch",
   },
   {
+    id: 4,
     name: "Teriyaki Tofu",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768306/Recipe-App/lunch/ronise-daluz-LgTyii0GDKQ-unsplash_w656wa.jpg",
     cal: 320,
+    category: "lunch",
   },
   {
+    id: 5,
     name: "Black Beans",
     img: "https://res.cloudinary.com/dxsom7jmx/image/upload/v1680768373/Recipe-App/lunch/k8-w8OCrTBTaMc-unsplash_ihuj0c.jpg",
     cal: 250,
+    category: "lunch",
   },
 ];
